e56e825172631bc0e0840781f1751f334cff6f92
lesmils <miltontsie@hotmail.com>
2022-03-20 21:53:10 +0100

    posting liveEvents

--- backendProject/routers/liveEvents.js
+++ backendProject/routers/liveEvents.js
@@ -1,8 +1,7 @@
 const { Router } = require("express");
 const User = require("../models").user;
-const Category = require("../models").category;
-const SkillTag = require("../models").skillTag;
 const LiveEvent = require("../models").liveEvent;
+const authMiddleware = require("../auth/middleware");
 const router = new Router();
 
 router.get("/", async (req, res) => {
@@ -16,6 +15,39 @@ router.get("/", async (req, res) => {
     order: [["time", "ASC"]],
   });
   res.status(200).send(allLiveEvents);
+});
+
+router.post("/", authMiddleware, async (req, res) => {
+  const {
+    name,
+    time,
+    durationHours,
+    description,
+    location,
+    imageUrl,
+    tokenCost,
+    categoryId,
+    maxParticipants,
+  } = req.body;
+
+  const user = req.user;
+
+  //this take the info of the user
+  //   console.log("what is user", user);
+
+  const newLiveEvent = await LiveEvent.create({
+    name: name,
+    time: time,
+    durationHours: durationHours,
+    description: description,
+    location: location,
+    imageUrl: imageUrl,
+    tokenCost: tokenCost,
+    categoryId: categoryId,
+    maxParticipants: maxParticipants,
+    organizerId: user.id,
+  });
+  res.status(200).send(newLiveEvent);
 });
 
 module.exports = router;
